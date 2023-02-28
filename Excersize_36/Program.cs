c2d0beb2ce1c20b5e321d5d6156e51db5d6d63f0
Valentina <valush31@gmail.com>
2023-02-28 16:33:54 +0300

    add Excersize 36

---
+++ Excersize_36/Program.cs
@@ -0,0 +1,43 @@
+﻿//Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+//[3, 7, 23, 12] -> 19
+//[-4, -6, 89, 6] -> 0 
+
+int[] CreateRandomArray(int sizeArray, int start, int end)
+{
+    int[] randomArray = new int[sizeArray];
+    for (int i = 0; i < sizeArray; i++)
+    {
+        randomArray[i] = new Random().Next(start, end);
+    }
+    return randomArray;
+}
+
+void ShowArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+int SumOfElementsAttOddPosition(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 != 0)
+        {
+            sum += array[i];
+        }
+    }
+    return sum;
+}
+
+int[] array = CreateRandomArray(6, 0, 100);
+ShowArray(array);
+
+int sum = SumOfElementsAttOddPosition(array);
+Console.WriteLine($"Сумма элементов массива, стоящих на нечётных позициях = {sum}");
+
